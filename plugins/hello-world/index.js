8d1f4851c52023ce1699f80f73cf61ea3be60189
Lukas Holzer <lukas.holzer@typeflow.cc>
2021-12-09 14:22:01 +0100

    update logging in build plugin

--- plugins/hello-world/index.js
+++ plugins/hello-world/index.js
@@ -4,10 +4,10 @@ module.exports = {
       build: { environment },
     },
   }) => {
-    console.log(process.env.BUILD_ID);
-    console.log(process.env.DEPLOY_ID);
-    console.log(process.env.DEPLOY_PRIME_URL);
-    console.log(process.env.DEPLOY_URL);
+    console.log('BUILD_ID: ', process.env.BUILD_ID);
+    console.log('DEPLOY_ID: ', process.env.DEPLOY_ID);
+    console.log('DEPLOY_PRIME_URL: ', process.env.DEPLOY_PRIME_URL);
+    console.log('DEPLOY_URL: ', process.env.DEPLOY_URL);
 
     console.log(environment);
   },
