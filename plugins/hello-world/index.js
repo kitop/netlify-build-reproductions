cbb430e68c6bd3e8a3d67669f52b879c918ec42a
Lukas Holzer <lukas.holzer@typeflow.cc>
2021-12-10 13:53:41 +0100

    update build plugin

--- plugins/hello-world/index.js
+++ plugins/hello-world/index.js
@@ -1,14 +1,8 @@
 module.exports = {
-  onPreBuild: ({
-    netlifyConfig: {
-      build: { environment },
-    },
-  }) => {
+  onPreBuild: (config) => {
     console.log('BUILD_ID: ', process.env.BUILD_ID);
     console.log('DEPLOY_ID: ', process.env.DEPLOY_ID);
     console.log('DEPLOY_PRIME_URL: ', process.env.DEPLOY_PRIME_URL);
     console.log('DEPLOY_URL: ', process.env.DEPLOY_URL);
-
-    console.log(environment);
   },
 };
---
+++ tests/hello.test.js
@@ -0,0 +1,6 @@
+const test = require('ava')
+
+
+test('hello', (t) => {
+  t.is('hello', 'hello')
+})
